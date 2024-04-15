71fcb6889c8fc31f7d975bcccc0118cb86f72a0c
LeMathoux <123992331+LeMathoux@users.noreply.github.com>
2024-04-15 11:29:02 +0200

    Update configdb.php ajout de la configuration

--- requetes/configdb.php
+++ requetes/configdb.php
@@ -1,1 +1,13 @@
+<?php
 
+$username = 'root';
+$password = '';
+$name = 'mathindex';
+try
+{
+  $mysqlClient = new PDO("mysql:host=127.0.0.1; dbname=$name", $username, $password);
+}
+catch (Exception $e){
+  die('Erreur : ' . $e->getMessage());
+}
+?>
