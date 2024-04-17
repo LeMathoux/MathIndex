4cfae5548f78cc4e544f3d1409be012b88276e1c
MelisandeOnana <160874940+MelisandeOnana@users.noreply.github.com>
2024-04-17 09:48:01 +0200

    Update Exercices.php

--- Exercices.php
+++ Exercices.php
@@ -1,7 +1,18 @@
 <?php 
 session_start();
 // Connexion à la base de données (à remplacer avec vos informations de connexion)
-include_once 'requetes/configdb.php';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mathindex";
+
+// Création de la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérification de la connexion
+if ($conn->connect_error) {
+    die("La connexion a échoué : " . $conn->connect_error);
+}
 
 // Pagination
 $exercices_par_page = 5;
@@ -25,11 +36,352 @@ $total_pages = ceil($total_exercices / $exercices_par_page);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href='assets/styles/Exercices.css' rel='stylesheet'/>
   <title>Exercices</title>
+  <style>
+   body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+  background-color: #eff2f4;
+}
+
+.barre-navigation {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 297px;
+  background-color: #FFFFFF;
+  padding-top: 15px;
+  box-shadow: 5px 0px 15px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  display: flex; /* Ajout */
+  flex-direction: column; /* Ajout */
+  align-items: center; /* Ajout */
+}
+
+.barre-navigation li {
+  list-style: none;
+  margin-left: 16px;
+  line-height:2;
+}
+
+.navigation{
+    padding-top:75px;
+}
+
+.barre-navigation ul li {
+  padding: 10px 20px;
+}
+
+.barre-navigation strong{
+    margin-left:6px;
+}
+
+.barre-navigation a.accueil-liens {
+    color: #757575;
+    display: flex;
+    align-items: center;
+    text-decoration:none;
+}
+
+.barre-navigation a.recherche-liens {
+    color: #757575;
+    display: flex;
+    align-items: center;
+    text-decoration:none;
+}
+
+.barre-navigation a.fonctions-liens {
+    background-color: #F6F6F6;
+    color: #1B3168;
+    width: 253.38px;
+    height: 38.64px;
+    padding: 0px 7.32px 0px 7.32px;
+    border-radius: 3.66px;
+    display: flex;
+    align-items: center;
+    text-decoration:none;
+}
+
+.barre-navigation a.mesexercices-liens {
+    color: #757575;
+    display: flex;
+    align-items: center;
+    text-decoration:none;
+}
+
+.barre-navigation a.soumettre-liens {
+    color: #757575;
+    display: flex;
+    align-items: center;
+    text-decoration:none;
+}
+
+.deconnexion .deconnexion-liens{
+  color: #757575;
+  display: flex;
+    align-items: center;
+    text-decoration:none;
+    font-size:14px;
+    background-color:#F6F6F6;
+    width:253px;
+    height:38px;
+  }
+
+.deconnexion{
+  margin-top: 239%;
+}
+
+.barre-navigation a img {
+    margin-right: 10px;
+}
+
+.ensembles-logo  {
+  display: flex;
+  position: relative;
+  margin-left: 15px;
+}
+
+        .ensembles-logo-titre img{
+            width: 32px;
+            height: 34px;
+        }
+
+        .ensembles-logo-titre {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            margin-left: 15px;
+        }
+
+        .ensembles-logo-titre .titre{
+            color: #1B3168;
+            font-size: 26px;
+        }
+
+        .ensembles-logo-titre .sous-titre {
+            font-size: 17px;
+            font-weight: 500;
+            color: #5D7285;
+        }
+
+header {
+  background-color: #FFFFFF;
+  position: relative;
+  width: 100%;
+  height: 96px;
+}
+
+.header-droite {
+  position: absolute;
+  top: 50%;
+  right: 20px;
+  transform: translateY(-50%);
+}
+
+.header-droite a {
+  text-decoration: none;
+  color: #333;
+  padding: 10px 20px;
+  display: flex;
+    align-items: center;
+}
+
+.header-droite img{
+    margin-right: 10px;
+    width: 21px;
+    height: 20px;
+}
+
+.contenu {
+  padding-top: 15px;
+}
+
+.contenu h1{
+    margin-left: 19%;
+    color: #1B3168;
+    font-size:28px;
+}
+
+.carre-blanc  {
+    width: 79%;
+    height: 832px;
+    background-color: #FFFFFF;
+    border-radius: 7.36px;
+    position: absolute;
+    top: 605px;
+    left: 58%;
+    transform: translate(-50%, -50%);
+}
+
+.carre-blanc h2{
+            font-size: 22px;
+            padding-left: 50px;
+            padding-top: 24px;
+        }
+
+.carre-blanc table {
+            width: 90%;
+            height: 44px;
+            border-collapse: collapse;
+            border-radius: 5px;
+            overflow: hidden; /* Pour que les coins arrondis soient visibles */
+            margin-left: 48px;
+}
+
+.carre-blanc thead {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+            background-color: #E7E7E7;
+            color: #464646;
+}
+
+.carre-blanc td, th {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+            color: #464646;
+}
+
+.carre-blanc td a{
+    text-decoration:none;
+    color:#74828F;
+    margin-right: 5px;
+}
+
+.carre-blanc td img{
+    margin-right:5px;
+    position: relative;
+    top: 5px;
+}
+
+.carre-blanc .mot_cle {
+    display: inline-block;
+    padding: 5px 10px;
+    margin: 5px;
+    border-radius: 20px;
+    background-color: #DBDBDB;
+    border:none;
+}
+
+.carre-blanc .pagination {
+            margin-top: 20px;
+            display: flex;
+            justify-content: center;
+}
+
+.pagination a{
+            margin: 0 5px;
+            width:24px;
+            height:24px;
+            color: black;
+            border: 1px solid #DFE3E8;
+            border-radius:4px;
+            text-decoration:none;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+}
+
+span.page-actuel{
+            border: 1px solid black; /* Ajout de la bordure par défaut */
+            margin: 0 5px;
+            width: 24px;
+            height: 24px;
+            border-radius: 5px;
+            color: black;
+            text-decoration: none;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+}
+
+.pagination .page-actuel:hover {
+    border: 1px solid black; 
+}
+
+a.pagination-bouton-gauche{
+            border: 1px solid #DFE3E8;
+            margin: 0 5px;
+            width: 24px;
+            height: 24px;
+            border-radius: 5px;
+            color: #C4CDD5;
+            text-decoration:none;
+            display:flex;
+            align-items:center;
+            justify-content: center;
+}
+
+span.pagination-bouton-gauche{
+            border:none;
+            margin: 0 5px;
+            width: 26px;
+            height: 26px;
+            border-radius: 5px;
+            color: #C4CDD5;
+            background-color:#919EAB;
+            text-decoration:none;
+            display:flex;
+            align-items:center;
+            justify-content: center;
+}
+
+span.pagination-bouton-droite{
+            border:none;
+            margin: 0 5px;
+            width: 26px;
+            height: 26px;
+            border-radius: 5px;
+            color: #C4CDD5;
+            background-color:#919EAB;
+            text-decoration:none;
+            display:flex;
+            align-items:center;
+            justify-content: center;
+}
+
+a.pagination-bouton-droite{
+            border: 1px solid #DFE3E8;
+            margin: 0 5px;
+            width: 24px;
+            height: 24px;
+            border-radius: 5px;
+            color: #C4CDD5;
+            text-decoration:none;
+            display:flex;
+            align-items:center;
+            justify-content: center;
+}
+
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+.mentionlegales {
+  font-size: 20px;
+  color: #000000;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  margin-left: 350px;
+  margin-top: 31.3px;
+  margin-bottom: 9px;
+}
+
+.mentionlegales-text {
+  display: inline-block;
+  margin: 0 10px;
+  font-size: 20px;
+}
+  </style>
 </head>
 <body>
-<nav class="barre-navigation">
+    <nav class="barre-navigation">
         <div class="ensembles-logo">
             <img alt="logo" src="assets/images/Logo.svg">
             <div class="ensembles-logo-titre ">
@@ -41,14 +393,11 @@ $total_pages = ceil($total_exercices / $exercices_par_page);
             <li><a href="Accueil.php" class="accueil-liens"><img src="assets/images/icone_home_gris.svg">Accueil</a></li>
             <li><a href="Recherche.php" class="recherche-liens"><img src="assets/images/icone_search_gris.svg">Recherche</a></li>
             <li><a href="Exercices.php" class="fonctions-liens"><img src="assets/images/icone_fonctions.svg"><strong>Exercices</strong></a></li>
-            <?php 
-            if(isset($_SESSION["account"]) &&(($_SESSION["account"]['role'] === 'Administrateur') || ($_SESSION["account"]['role'] === 'Contributeur'))){
-                echo '<li><a href="MesExercices.php" class="mesexercices-liens"><img src="assets/images/icone_liste_gris.svg">Mes exercices</a></li>
-                <li><a href="Soumettre-information_generales.php" class="soumettre-liens"><img src="assets/images/icone_soumettre_gris.svg">Soumettre</a></li>';
-            } ?>
+            <li><a href="MesExercices.php" class="mesexercices-liens"><img src="assets/images/icone_liste_gris.svg">Mes exercices</a></li>
+            <li><a href="Soumettre-information_generales.php" class="soumettre-liens"><img src="assets/images/icone_soumettre_gris.svg">Soumettre</a></li>
             <div class="deconnexion">
                 <?php if(isset($_SESSION["account"])): ?>
-                    <li><a href="requetes/logout.php" class="deconnexion-liens"><img src="assets/images/icone_logout.svg">Déconnexion</a></li>
+                    <li><a href="logout.php" class="deconnexion-liens"><img src="assets/images/icone_logout.svg">Déconnexion</a></li>
                 <?php endif; ?>
             </div>
         </div>
@@ -115,10 +464,10 @@ $total_pages = ceil($total_exercices / $exercices_par_page);
                                 echo '</td>';
                                 echo "<td>";
                                 echo "<img src='assets/images/icone_download.svg'>
-                                      <a href='assets/Exercices/" . $row["exercice_original_name"] . "." . $row["extension"] . "' target='_blank'>Exercice</a>";
+                                      <a href='assets/Exercices/" . $row["exercice_original_name"] . "." . $row["extension"] . "' download>Exercice</a>";
                                 if ($row["correction_original_name"] && $row["correction_extension"]) {
                                     echo "<br><img src='assets/images/icone_download.svg'>
-                                          <a href='assets/Corrigé/" . $row["correction_original_name"]. "." . $row["correction_extension"] . "' target='_blank'>Corrigé</a>";
+                                          <a href='assets/Corrigé/" . $row["correction_original_name"]. "." . $row["correction_extension"] ."' download>Corrigé</a>";
                                 }
                                 echo "</td>";
                                 echo "</tr>";
@@ -170,10 +519,10 @@ $total_pages = ceil($total_exercices / $exercices_par_page);
                                     echo '</td>';
                                     echo "<td>";
                                     echo "<img src='assets/images/icone_download.svg'>
-                                          <a href='assets/Exercices/" . $row["exercice_original_name"] . "." . $row["extension"] . "' target='_blank'>Exercice</a>";
+                                          <a href='assets/Exercices/" . $row["exercice_original_name"] . "." . $row["extension"] . "' download>Exercice</a>";
                                     if ($row["correction_original_name"] && $row["correction_extension"]) {
                                         echo "<br><img src='assets/images/icone_download.svg'>
-                                              <a href='assets/Corrigé/" . $row["correction_original_name"]. "." . $row["correction_extension"] . "' target='_blank'>Corrigé</a>";
+                                              <a href='assets/Corrigé/" . $row["correction_original_name"]. "." . $row["correction_extension"] . "' download>Corrigé</a>";
                                     }
                                     echo "</td>";
                                     echo "</tr>";
