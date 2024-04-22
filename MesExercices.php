fa42cf9b666226d70ec4d7fc345880b4acc1d4e1
AllanDe9 <163830814+AllanDe9@users.noreply.github.com>
2024-04-22 08:44:46 +0200

    Update MesExercices.php

--- MesExercices.php
+++ MesExercices.php
@@ -39,13 +39,13 @@ $total_pages = ceil($total_exercices / $exercices_par_page);
         </div>
     </div>
     <div class="navigation">
-        <li><a href="Accueil.php" class="fonctions-liens"><img src="assets/images/icone_home.svg">Accueil</a></li>
+        <li><a href="Accueil.php" class="fonctions-liens"><img src="assets/images/icone_home_gris.svg">Accueil</a></li>
         <li><a href="Recherche.php" class="recherche-liens"><img src="assets/images/icone_search_gris.svg">Recherche</a></li>
         <li><a href="Exercices.php" class="fonctions-liens"><img src="assets/images/icone_fonctions_gris.svg">Exercices</a></li>
-        <li><a href="MesExercices.php" class="mesexercices-liens"><img src="assets/images/icone_liste_gris.svg">Mes exercices</a></li>
+        <li><a href="MesExercices.php" class="mesexercices-liens"><img src="assets/images/icone_liste.svg">Mes exercices</a></li>
         <li><a href="Soumettre-information_generales.php" class="soumettre-liens"><img src="assets/images/icone_soumettre_gris.svg">Soumettre</a></li>
         <div class="deconnexion">
-            <li><a href="logout.php" class="deconnexion-liens"><img src="assets/images/icone_logout.svg">Déconnexion</a></li>
+            <li><a href="requetes/logout.php" class="deconnexion-liens"><img src="assets/images/icone_logout.svg">Déconnexion</a></li>
         </div>
     </div>
   </nav>
@@ -64,78 +64,83 @@ $total_pages = ceil($total_exercices / $exercices_par_page);
   <div class="contenu">
     <h1>Mes Exercices</h1>
     <div class="carre-blanc">
-        <h2>Vous pouvez modifier ou supprimer un de vos excercices</h2>
-        <table>
-            <thead>
-                <td><p>Nom</p></td>
-                <td><p>Thématiques</p></td>
-                <td><p>Fichiers</p></td>
-                <td><p>Actions</p></td>
-            </thead>
-            <?php
-            $sql_all_exercices = "SELECT exercise.name AS exercise_name, thematic.name AS thematic_name, file_exercice.original_name AS exercice_original_name, file_exercice.extension, file_correction.original_name AS correction_original_name, file_correction.extension AS correction_extension, exercise.id AS exercise_id
-                                  FROM exercise
-                                  LEFT JOIN thematic ON exercise.thematic_id = thematic.id
-                                  LEFT JOIN file AS file_exercice ON exercise.exercice_file_id = file_exercice.id
-                                  LEFT JOIN file AS file_correction ON exercise.correction_file_id = file_correction.id
-                                  WHERE created_by_id = $iduser
-                                  LIMIT $exercices_par_page OFFSET $offset";
-
-            $result_all_exercices = $conn->query($sql_all_exercices);
-
-            if ($result_all_exercices->num_rows > 0) {
-              while ($row = $result_all_exercices->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td class='nom'><p>" . $row["exercise_name"] . "</p></td>";
-                echo "<td class='thematiques'><p>" . $row["thematic_name"] . "</p></td>";
-                echo "<td class='fichiers'>";
-                echo "<img src='assets/images/icone_download.svg'>
-                      <a href='assets/Exercices/" . $row["exercice_original_name"] . "." . $row["extension"] . "' download>Exercice</a>";
-                if ($row["correction_original_name"] && $row["correction_extension"]) {
-                  echo "<img src='assets/images/icone_download.svg'>
-                        <a href='assets/Corrigé/" . $row["correction_original_name"]. "." . $row["correction_extension"] ."' download>Corrigé</a>";
-                }
-                echo "</td>";
-                echo "<td class='actions'>";
-                echo "<img src='assets/images/icone_modifier_gris.svg'>
-                      <p><a href=''>Modifier</a></p>";
-                echo "<img src='assets/images/icone_poubelle_gris.svg'>
-                      <p><a href=''>Supprimer</a></p>";
-                echo "</td>";
-                echo "</tr>";
-              }
-            } 
-            $sql_no_exercices = "SELECT COUNT(*) AS NbreEx FROM exercise WHERE created_by_id = $iduser";
+      <?php $sql_no_exercices = "SELECT COUNT(*) AS NbreEx FROM exercise WHERE created_by_id = $iduser";
             $result_no_exercice = $conn->query($sql_no_exercices);
-            if ($result_no_exercice = 0) {
-              echo "<p>Vous n'avez pas ajouté d'exercices.</p>";
-            }
+            $row = $result_no_exercice->fetch_assoc();
+            $count = $row['NbreEx'];
+            if ($count == 0) {
+              echo "<tr><td><p>Vous n'avez pas ajouté d'exercices.</p></td></tr>";
+            } else { 
+                  ?>
+                  <h2>Vous pouvez modifier ou supprimer un de vos excercices</h2>
+                  <table>
+                      <thead>
+                          <td><p>Nom</p></td>
+                          <td><p>Thématiques</p></td>
+                          <td><p>Fichiers</p></td>
+                          <td><p>Actions</p></td>
+                      </thead>
+                  <?php
+                  $sql_all_exercices = "SELECT exercise.name AS exercise_name, thematic.name AS thematic_name, file_exercice.original_name AS exercice_original_name, file_exercice.extension, file_correction.original_name AS correction_original_name, file_correction.extension AS correction_extension, exercise.id AS exercise_id
+                                        FROM exercise
+                                        LEFT JOIN thematic ON exercise.thematic_id = thematic.id
+                                        LEFT JOIN file AS file_exercice ON exercise.exercice_file_id = file_exercice.id
+                                        LEFT JOIN file AS file_correction ON exercise.correction_file_id = file_correction.id
+                                        WHERE created_by_id = $iduser
+                                        LIMIT $exercices_par_page OFFSET $offset";
+
+                  $result_all_exercices = $conn->query($sql_all_exercices);
+
+                  if ($result_all_exercices->num_rows > 0) {
+                    while ($row = $result_all_exercices->fetch_assoc()) {
+                      echo "<tr>";
+                      echo "<td class='nom'><p>" . $row["exercise_name"] . "</p></td>";
+                      echo "<td class='thematiques'><p>" . $row["thematic_name"] . "</p></td>";
+                      echo "<td class='fichiers'>";
+                      echo "<img src='assets/images/icone_download.svg'>
+                            <a href='assets/Exercices/" . $row["exercice_original_name"] . "." . $row["extension"] . "' download>Exercice</a>";
+                      if ($row["correction_original_name"] && $row["correction_extension"]) {
+                        echo "<img src='assets/images/icone_download.svg'>
+                        <a href='assets/Corrigé/" . $row["correction_original_name"]. "." . $row["correction_extension"] ."' download>Corrigé</a>";
+                      }
+                      echo "</td>";
+                      echo "<td class='actions'>";
+                      echo "<img src='assets/images/icone_modifier_gris.svg'>
+                            <p><a href=''>Modifier</a></p>";
+                      echo "<img src='assets/images/icone_poubelle_gris.svg'>
+                            <p><a href='?page=".$_GET['page']."&action=delete'>Supprimer</a></p>";
+                      echo "</td>";
+                     echo "</tr>";
+                    }
+                  } 
+                  echo "</table>";         
+          }
+        if ($count != 0) { 
+        ?>
+          <div class="pagination">
+            <?php
+              if ($page > 1) {
+                echo "<a href='MesExercices.php?page=".($page - 1)."' class='pagination-bouton-gauche'>&lt;</a>";
+              } else {
+                echo "<span class='pagination-bouton-gauche'>&lt;</span>";
+              }
+
+              for ($i=1; $i<=$total_pages; $i++) {
+                 if ($i == $page) {
+                   echo "<span class='page-actuel'>$i</span>";
+                  } else {
+                    echo "<a href='MesExercices.php?page=".$i."' class='pagination-lien'>$i</a>";
+                  }
+              }
+
+              if ($page < $total_pages) {
+                echo "<a href='MesExercices.php?page=".($page + 1)."' class='pagination-bouton-droite'>&gt;</a>";
+              } else {
+                echo "<span class='pagination-bouton-droite'>&gt;</span>";
+              }
             ?>
-        </table>
-        <?php echo $result_no_exercice; ?>
-        <div class="pagination">
-                    <?php
-                        if ($page > 1) {
-                            echo "<a href='MesExercices.php?page=".($page - 1)."' class='pagination-bouton-gauche'>&lt;</a>";
-                        } else {
-                            echo "<span class='pagination-bouton-gauche'>&lt;</span>";
-                        }
-
-                        for ($i=1; $i<=$total_pages; $i++) {
-                            if ($i == $page) {
-                                echo "<span class='page-actuel'>$i</span>";
-                            } else {
-                                echo "<a href='MesExercices.php?page=".$i."' class='pagination-lien'>$i</a>";
-                            }
-                        }
-
-                        if ($page < $total_pages) {
-                            echo "<a href='MesExercices.php?page=".($page + 1)."' class='pagination-bouton-droite'>&gt;</a>";
-                        } else {
-                            echo "<span class='pagination-bouton-droite'>&gt;</span>";
-                        }
-                    ?>
-                </div>
+          </div>
+        <?php } ?>
     </div>
   </div>
   <footer>
@@ -147,7 +152,17 @@ $total_pages = ceil($total_exercices / $exercices_par_page);
       <div class="mentionlegales-text">Lycée Saint-Vincent</div>
     </div>
   </footer>
-
+  <?php
+    if (isset($_GET['action']) && $_GET['action'] === 'delete') {
+        echo '<div class="modal">';
+        echo '<div class="modal-content">';
+        echo '<p>Voulez-vous vraiment supprimer cet élément ?</p>';
+        echo '<a href="?confirmed=true" class="confirm-btn">Oui</a>';
+        echo '<a href="?confirmed=false" class="cancel-btn">Annuler</a>';
+        echo '</div>';
+        echo '</div>';
+    }
+    ?>
 </body>
 </html>
 
